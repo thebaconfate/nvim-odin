51d019d99aa8739addbee874d1066ff0af783d75
Gérard Lichtert <gerard.lichtert@hotmail.com>
2024-09-10 18:27:25 -0400

    added git

--- lua/odin/set.lua
+++ lua/odin/set.lua
@@ -10,3 +10,14 @@ vim.o.softtabstop = 4
 vim.o.expandtab = true
 vim.o.smartindent = true
 
+vim.o.hlsearch = false
+vim.o.incsearch = true
+
+vim.o.scrolloff = 8
+vim.o.signcolumn = "yes"
+
+vim.o.updatetime = 50
+vim.o.colorcolumn = "80"
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader ='\\'
