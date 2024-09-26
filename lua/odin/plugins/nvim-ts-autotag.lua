289f6b3f02b911189f11041b90d9b657a0efeb94
Gérard Lichtert <gerard.lichtert@hotmail.com>
2024-09-26 22:25:35 +0200

    added tsx auto tags

---
+++ lua/odin/plugins/nvim-ts-autotag.lua
@@ -0,0 +1,7 @@
+return {
+	"windwp/nvim-ts-autotag",
+	config = function()
+		require("nvim-ts-autotag").setup({})
+	end,
+	ft = { "html", "javascriptreact", "typescriptreact", "svelte", "vue", "xml" },
+}
