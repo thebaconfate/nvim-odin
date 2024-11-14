c56feca543f986490b52d281d703ee72eb7f2d7b
Gérard Lichtert <gerard.lichtert@hotmail.com>
2024-11-14 17:48:31 +0100

    added stuff

--- lua/odin/plugins/nvim-treesitter.lua
+++ lua/odin/plugins/nvim-treesitter.lua
@@ -1,44 +1,46 @@
 -- nvim-treesitter configuration
 return {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate', -- Automatically update parsers
-    config = function()
-        require 'nvim-treesitter.configs'.setup {
-            ensure_installed = {
-                -- Specify the languages you want
-                "c",
-                "lua",
-                "python",
-                "javascript",
-                "typescript",
-                "tsx",
-                "scala",
-                "css",
-                "html",
-                "java",
-                "astro",
-                "latex"
-            },
-            highlight = {
-                enable = true, -- Enable highlighting
-                disable = {}, -- List of languages to disable (if any)
-            },
-            indent = {
-                enable = true, -- Enable indentation
-            },
-            -- Optional additional modules (text objects, playground, etc.)
-            textobjects = {
-                select = {
-                    enable = true,
-                    lookahead = true,
-                    keymaps = {
-                        ["af"] = "@function.outer",
-                        ["if"] = "@function.inner",
-                        ["ac"] = "@class.outer",
-                        ["ic"] = "@class.inner",
-                    },
-                },
-            },
-        }
-    end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate", -- Automatically update parsers
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				-- Specify the languages you want
+				"c",
+				"lua",
+				"python",
+				"javascript",
+				"typescript",
+				"tsx",
+				"scala",
+				"css",
+				"html",
+				"java",
+				"astro",
+				"latex",
+				"yaml",
+				"dockerfile",
+			},
+			highlight = {
+				enable = true, -- Enable highlighting
+				disable = {}, -- List of languages to disable (if any)
+			},
+			indent = {
+				enable = true, -- Enable indentation
+			},
+			-- Optional additional modules (text objects, playground, etc.)
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ic"] = "@class.inner",
+					},
+				},
+			},
+		})
+	end,
 }
