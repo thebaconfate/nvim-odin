ee62de35294b86b6f13a7a60571caf98deb5b770
Gérard Lichtert <62557229+thebaconfate@users.noreply.github.com>
2024-12-20 06:43:20 +0100

    updated config

--- lua/odin/plugins/nvim-metals.lua
+++ lua/odin/plugins/nvim-metals.lua
@@ -5,31 +5,45 @@ return {
     ft = { "scala", "sbt" },
     dependencies = {
         "nvim-lua/plenary.nvim",
+        {
+            "j-hui/fidget.nvim",
+            opts = {},
+        },
     },
-    -- stylua: ignore
-    keys = {
-        { "<leader>cW", function () require('metals').hover_worksheet() end, desc = "Metals Worksheet" },
-        { "<leader>cM", function () require('telescope').extensions.metals.commands() end, desc = "Telescope Metals Commands" },
-    },
-    config = function()
+    opts = function()
         local metals_config = require("metals").bare_config()
-
         metals_config.settings = {
             showImplicitArguments = true,
-            showImplicitConversionsAndClasses = true,
-            showInferredType = true,
-            superMethodLensesEnabled = true,
         }
-        metals_config.init_options.statusBarProvider = "on"
+        metals_config.init_options.statusBarProvider = "off"
         metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
-
+        metals_config.on_attach = function(client, bufnr)
+            if client.name == "metals" then
+                local map = vim.keymap.set
+                map("n", "<leader>gd", vim.lsp.buf.definition)
+                map("n", "<leader>gi", vim.lsp.buf.implementation)
+                map("n", "K", vim.lsp.buf.hover)
+                map("n", "<leader>vws", vim.lsp.buf.workspace_symbol)
+                map("n", "<leader>vd", vim.lsp.diagnostic.open_float)
+                map("n", "<leader>vca", vim.lsp.buf.code_action)
+                map("n", "<leader>vrr", vim.lsp.buf.references)
+                map("n", "<leader>vrn", vim.lsp.buf.rename)
+                map("i", "<C-h>", vim.lsp.buf.signature_help)
+                map("n", "nd", vim.lsp.diagnostic.goto_next)
+                map("n", "Nd", vim.lsp.diagnostic.goto_prev)
+            end
+        end
+        return metals_config
+    end,
+    config = function(self, metals_config)
         local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
         vim.api.nvim_create_autocmd("FileType", {
-            pattern = { "scala", "sbt" },
+            pattern = self.ft,
             callback = function()
                 require("metals").initialize_or_attach(metals_config)
             end,
             group = nvim_metals_group,
         })
-    end,
+    end
+
 }
