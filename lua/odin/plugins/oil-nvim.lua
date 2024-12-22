df75c542a9705dc8506b7df5b82d2ba7683433e6
Gérard Lichtert <62557229+thebaconfate@users.noreply.github.com>
2024-12-22 03:39:43 +0100

    replaced nvim-tree by oil

---
+++ lua/odin/plugins/oil-nvim.lua
@@ -0,0 +1,17 @@
+return {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+    config = function()
+        require("oil").setup({
+            default_file_explorer = true,
+            columns = { "icon" }
+        })
+
+        vim.keymap.set("n", "<leader>pv", ":Oil<CR>", { desc = "Open Oil" })
+    end
+}
