0f8d424839d9f6860c6d16f06c349d84e6868d31
Gérard Lichtert <gerard.lichtert@hotmail.com>
2026-01-06 09:44:21 +0100

    added some messaging

--- lua/odin/plugins/lsp-config.lua
+++ lua/odin/plugins/lsp-config.lua
@@ -24,13 +24,21 @@ return {
             if vim.fn.has("win32") == 0 then
                 return vim.env.SHELL or "/bin/bash"
             end
-            local sys_bash = "C:\\windows\\system32\\bash.exe"       -- WSL bash
+            local wsl_bash = "C:\\windows\\system32\\bash.exe"       -- WSL bash
             local git_bash = "C:\\Program Files\\Git\\bin\\bash.exe" -- Use Git Bash
-            if file_exists(sys_bash) then
-                return sys_bash
+            if file_exists(wsl_bash) then
+                return wsl_bash
             elseif file_exists(git_bash) then
+                vim.notify(
+                    "No WSL bash found, using git bash instead. Consider installing wsl for future use",
+                    vim.log.levels.WARN
+                )
                 return git_bash
             else
+                vim.notify(
+                    "No WSL or Git Bash found! Some features may not work.",
+                    vim.log.levels.WARN
+                )
                 return nil
             end
         end
