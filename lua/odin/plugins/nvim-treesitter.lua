b664671a2693fcf58cdca9b427c0a88bac800708
Gérard Lichtert <gerard.lichtert@hotmail.com>
2026-03-16 16:01:22 +0100

    updated dependencies

--- lua/odin/plugins/nvim-treesitter.lua
+++ lua/odin/plugins/nvim-treesitter.lua
@@ -11,6 +11,7 @@ return {
                 "python",
                 "javascript",
                 "typescript",
+                "jsx",
                 "tsx",
                 "scala",
                 "css",
