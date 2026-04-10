3ae135c07a2bd360311f78ea64008f0dfe45405d
Gérard Lichtert <gerard.lichtert@hotmail.com>
2026-04-10 02:01:21 +0200

    modified rainbow delimiter to only work on braces

--- lua/odin/plugins/rainbow-delimiters.lua
+++ lua/odin/plugins/rainbow-delimiters.lua
@@ -1,3 +1,10 @@
 return {
-    "hiphish/rainbow-delimiters.nvim"
+    "hiphish/rainbow-delimiters.nvim",
+    config = function()
+        vim.g.rainbow_delimiters = {
+            query = {
+                [''] = 'rainbow-parens', -- ONLY parentheses/brackets
+            },
+        }
+    end
 }
